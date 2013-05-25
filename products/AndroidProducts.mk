2c28aa8c6c91bd1ce9310b7ba7b27544dc5d632f
pio_masaki <piomasaki@gmail.com>
2013-05-24 21:43:18 -0700

    Add to AndroidProducts.mk and add xenon_dlx.mk device file

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -16,4 +16,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/xenonhd_vigor.mk\
     $(LOCAL_DIR)/xenonhd_xt925.mk\
     $(LOCAL_DIR)/xenonhd_m7.mk\
-    $(LOCAL_DIR)/xenonhd_xt926.mk 
+    $(LOCAL_DIR)/xenonhd_xt926.mk\
+    $(LOCAL_DIR)/xenonhd_dlx.mk
---
+++ products/xenon_dlx.mk
@@ -0,0 +1,32 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/xenonhd/configs/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := dlx
+
+# Inherit common Verizon Wireless Perms and Lib
+$(call inherit-product, vendor/xenonhd/configs/vzw.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/dlx/device_dlx.mk)
+
+# Inherit common build.prop overrides
+-include vendor/xenonhd/products/common_versions.mk
+
+# Device naming
+PRODUCT_DEVICE := dlx
+PRODUCT_NAME := xenonhd_dlx
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := HTC Droid DNA
+PRODUCT_MANUFACTURER := HTC
+
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_dlx BUILD_FINGERPRINT=verizon_wwe/dlx/dlx:4.1.1/JRO03C/147796.1:user/release-keys PRIVATE_BUILD_DESC="2.06.605.1 CL147796 release-keys" BUILD_NUMBER=123160
+
+PRODUCT_COPY_FILES += \
+    vendor/xenonhd/proprietary/dlx/media/bootanimation.zip:system/media/bootanimation.zip
+    
+# Inherit drm blobs
+-include vendor/xenonhd/products/common_drm.mk
+ 
